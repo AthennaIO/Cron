bee43f380b38378959db5c560210046ead147a56
João Lenon <lenonsec7@gmail.com>
2024-07-29 00:23:34 +0100

    Initial commit

---
+++ bin/test.ts
@@ -0,0 +1,17 @@
+/**
+ * @athenna/template
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { Runner } from '@athenna/test'
+
+await Runner.setTsEnv()
+  .addAssertPlugin()
+  .addPath('tests/unit/**/*.ts')
+  .setCliArgs(process.argv.slice(2))
+  .setGlobalTimeout(5000)
+  .run()
---
+++ tests/unit/TemplateTest.ts
@@ -0,0 +1,18 @@
+/**
+ * @athenna/template
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { Bootstrap } from '#src'
+import { Test, type Context } from '@athenna/test'
+
+export default class TemplateTest {
+  @Test()
+  public async shouldBeAbleToCreateAndRunTestsWithThisTemplate({ assert }: Context) {
+    assert.equal(Bootstrap.main('Hello', 'World!'), 'Bootstrap: Hello World!')
+  }
+}
