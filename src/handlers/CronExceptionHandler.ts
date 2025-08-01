0b28e2414f30479cd091ed1a50d68050f547c38d
jlenon7 <lenonSec7@gmail.com>
2025-08-01 15:00:37 -0300

    chore(npm): update dependencies

--- src/handlers/CronExceptionHandler.ts
+++ src/handlers/CronExceptionHandler.ts
@@ -52,27 +52,13 @@ export class CronExceptionHandler {
       return
     }
 
-    const isUsingJsonFormatter = Config.is(
-      'logging.channels.exception.formatter',
-      'json'
-    )
-
-    if (isUsingJsonFormatter) {
-      Log.channelOrVanilla('exception').error({
-        name: error.name,
-        code: error.code,
-        status: error.status,
-        message: error.message,
-        help: error.help,
-        cause: error.cause,
-        details: error.details,
-        stack: error.stack
-      })
+    if (Config.is('cron.logger.prettifyException', true)) {
+      Log.channelOrVanilla('exception').error(await error.prettify())
 
       return
     }
 
-    Log.channelOrVanilla('exception').error(await error.prettify())
+    Log.channelOrVanilla('exception').error(error)
   }
 
   /**
